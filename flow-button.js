a093b191cf544ad186d63659a626af798c138f97
gadgeski <xtwh.ce5bil@gmail.com>
2025-05-05 20:28:46 +0900

    Created index.html and more

---
+++ flow-button.js
@@ -0,0 +1,13 @@
+/* カーソルの位置で変色 */
+const btn = document.querySelector("#flow-button-js");
+btn.addEventListener("mousemove", (e) => {
+  const rect = btn.getBoundingClientRect();
+  const x = ((e.clientX - rect.left) / rect.width) * 100;
+  // カーソル位置の計算
+  btn.computedStyleMap.backgroundPosition = `${x}% 0 ;`;
+  // 背景位置を動的に
+});
+btn.addEventListener("mouseleave", () => {
+  btn.computedStyleMap.backgroundPosition = "0% 0";
+});
+// マウスを離した時
